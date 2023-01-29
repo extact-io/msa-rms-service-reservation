d322109e4ff5bcf66aae13725d555abb3b52eb55
ozytso <ozytso@gmail.com>
2023-01-29 22:27:35 +0900

    refactor: update around openapi

--- src/test/java/io/extact/msa/rms/reservation/it/external/stub/CheckApiRemoteStubApplication.java
+++ src/test/java/io/extact/msa/rms/reservation/it/external/stub/CheckApiRemoteStubApplication.java
@@ -1,20 +1,36 @@
 package io.extact.msa.rms.reservation.it.external.stub;
 
+import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
 
 import io.extact.msa.rms.platform.fw.login.LoginUserFromHttpHeadersRequestFilter;
-import io.extact.msa.rms.platform.fw.webapi.RmsApplication;
+import io.extact.msa.rms.platform.fw.webapi.RmsBaseApplications;
 
 @ApplicationPath("api")
-public class CheckApiRemoteStubApplication extends RmsApplication {
+public class CheckApiRemoteStubApplication extends Application {
+
     @Override
-    protected Set<Class<?>> getWebApiClasses() {
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new LinkedHashSet<>();
+        classes.addAll(RmsBaseApplications.CLASSES);
+        classes.addAll(getWebApiClasses());
+        return classes;
+    }
+
+    @Override
+    public Map<String, Object> getProperties() {
+        return RmsBaseApplications.PROPERTIES;
+    }
+
+    private Set<Class<?>> getWebApiClasses() {
         return Set.of(
-                LoginUserFromHttpHeadersRequestFilter.class,
-                RentalItemCheckApiRemoteStub.class,
-                UserAccountCheckApiRemoteStub.class
+                    LoginUserFromHttpHeadersRequestFilter.class,
+                    RentalItemCheckApiRemoteStub.class,
+                    UserAccountCheckApiRemoteStub.class
                 );
     }
 }
