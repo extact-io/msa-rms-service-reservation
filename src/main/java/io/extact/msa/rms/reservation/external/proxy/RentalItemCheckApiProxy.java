1f9fdadc5263b8662ceed7ccadd24ff386a402a0
extact-io <ozytso@gmail.com>
2023-05-22 22:54:52 +0900

    add: CircuitBreaker

--- src/main/java/io/extact/msa/rms/reservation/external/proxy/RentalItemCheckApiProxy.java
+++ src/main/java/io/extact/msa/rms/reservation/external/proxy/RentalItemCheckApiProxy.java
@@ -3,14 +3,22 @@ package io.extact.msa.rms.reservation.external.proxy;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
+import io.extact.msa.rms.platform.fw.exception.RmsNetworkConnectionException;
 import io.extact.msa.rms.platform.fw.exception.interceptor.NetworkConnectionErrorAware;
 import io.extact.msa.rms.reservation.external.RentalItemCheckApi;
 import io.extact.msa.rms.reservation.external.restclient.RentalItemCheckApiRestClient;
 
 @ApplicationScoped
 @NetworkConnectionErrorAware
+@CircuitBreaker(
+        requestVolumeThreshold = 4, 
+        failureRatio=0.5, 
+        delay = 10000, 
+        successThreshold = 3,
+        failOn = RmsNetworkConnectionException.class)
 public class RentalItemCheckApiProxy implements RentalItemCheckApi {
 
     private RentalItemCheckApiRestClient client;
--- src/main/java/io/extact/msa/rms/reservation/external/proxy/UserAccountCheckApiProxy.java
+++ src/main/java/io/extact/msa/rms/reservation/external/proxy/UserAccountCheckApiProxy.java
@@ -3,14 +3,22 @@ package io.extact.msa.rms.reservation.external.proxy;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
+import io.extact.msa.rms.platform.fw.exception.RmsNetworkConnectionException;
 import io.extact.msa.rms.platform.fw.exception.interceptor.NetworkConnectionErrorAware;
 import io.extact.msa.rms.reservation.external.UserAccountCheckApi;
 import io.extact.msa.rms.reservation.external.restclient.UserAccountCheckApiRestClient;
 
 @ApplicationScoped
 @NetworkConnectionErrorAware
+@CircuitBreaker(
+        requestVolumeThreshold = 4, 
+        failureRatio=0.5, 
+        delay = 10000, 
+        successThreshold = 3,
+        failOn = RmsNetworkConnectionException.class)
 public class UserAccountCheckApiProxy implements UserAccountCheckApi {
 
     private UserAccountCheckApiRestClient client;
